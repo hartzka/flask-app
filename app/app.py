6bb94dab8b3738f397ed6b3d28e921241de5995f
Kai Hartzell <kai.hartzell@gmail.com>
2020-10-30 19:14:01 +0200

    weatherdata

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 import pandas as pd
+import os
 
 app = Flask(__name__, template_folder="templates")
-df = pd.read_csv("data/weather.csv")
-dataset = df.to_html("detail.html")
+df = pd.read_csv(os.path.join(os.path.dirname(__file__),'data/weather.csv'))
+dataset = df.to_html("weather.html")
 
 @app.route("/")
 def index():
@@ -15,11 +16,10 @@ def infopage():
 
 @app.route("/weather")
 def hello():
-    data = dataset.html
-    return render_template("weather.html", data=data)
+    return render_template("weather.html")
 
 @app.route("/test")
 def test():
     return "Test"
 
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5000, debug=True)
