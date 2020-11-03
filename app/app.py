8bf1c5fccce5c7d0eb0130fcf6a4f8d6a4d0e739
Kai Hartzell <kai.hartzell@gmail.com>
2020-11-04 01:55:21 +0200

    cleaning

--- app/app.py
+++ app/app.py
@@ -43,8 +43,6 @@ current_weather = {'rain':[rain, data["Precipitation amount"]["units"]], 'temper
                    'snow': [snow, data["Snow depth"]["units"]], 'pressure': [pressure, data["Pressure (msl)"]["units"]],
                    'visibility': [visibility, data["Horizontal visibility"]["units"]], 'clouds': [clouds, data["Cloud amount"]["units"]]}
 
-print(current_weather)
-
 years = {2017, 2018, 2019}
 months = {1,2,3,4,5,6,7,8,9,10,11,12}
 days = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31}
